387f227634202a6caa3c2cb873e8d45bf5a0b190
thchan0 <1155143337@link.cuhk.edu.hk>
2022-02-22 21:56:09 +0800

    Edit line 18 for description

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          CUEx is the worse
+          CUExxxxxxxxxxxsdsssdsdddsd
         </a>
       </header>
     </div>
