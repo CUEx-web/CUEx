8b4ac0be6e8be7724450e25b5af6f55c2afd82ba
CKY531 <ken.chan531@yahoo.com.hk>
2022-03-19 23:26:50 +0800

    Add bodyParser and 404 Not found in backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 
 const express = require("express")
+const bodyParser = require("body-parser")
 const app = express()
 const mongoose = require("mongoose")
 
@@ -12,11 +13,20 @@ db.once("open", () => console.log("Connected to Database"))
 //Let our server accept json
 app.use(express.json())
 
+//Parse body
+app.use(bodyParser.urlencoded({extended: false}))
+
 const usersRouter = require("./routes/users")
 app.use("/users", usersRouter)
 
 const productsRouter = require("./routes/products")
 app.use("/products", productsRouter)
+
+//Return 404 Not Found if no middleware handles that req
+app.use((req, res, next) => {
+  //To do: Add the 404 not found page
+  res.status(404).send("<h1>Page not found</h1>");
+})
 
 app.listen(3000, () => console.log('Server Started'))
 
