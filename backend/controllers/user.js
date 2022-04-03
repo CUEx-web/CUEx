9932cc5a05898b33c91cfdfe4132fdc319eca03d
1155144002 <1155144002@link.cuhk.edu.hk>
2022-04-04 00:14:32 +0800

    Added more input options to getUsers()

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -4,8 +4,9 @@ const mongoose = require('mongoose');
 exports.getUsers = async (req, res) => {
     console.log(req.query)
     if (req.query.userName) {
+        // Return user with their userName matching req.query.userName
         Users.find({ "userName": req.query.userName })
-        .populate("productId")
+        .populate("productId", "__id productName")
         .then(user => {
             console.log(user)
             return res.status(200).json(user)
@@ -14,5 +15,34 @@ exports.getUsers = async (req, res) => {
             console.log(err)
             return res.status(500).json({ message: err.message });
         })
+    }
+    if (req.query.userId) {
+        // Return user with their _id matching req.query.userId
+        Users.find({ "_id": req.query.userId })
+        .populate("productId", "__id productName")
+        .then(user => {
+            console.log(user)
+            return res.status(200).json(user)
+        })
+        .catch(err => {
+            console.log(err)
+            return res.status(500).json({ message: err.message });
+        })
+    }
+    if (Object.keys(req.query).length == 0) {
+        // Empty query parameters means to return all users
+        Users.find()
+        .populate("productId", "__id productName")
+        .then(users => {
+            console.log(users)
+            return res.status(200).json(users)
+        })
+        .catch(err => {
+            console.log(err)
+            return res.status(500).json({ message: err.message });
+        })
+    } else if ((!req.query.userName) && (!req.query.userId)) {
+        // Return error if wrong query is done
+        return res.status(500).json({ message: "Call wrong api / Wrong query parameter" });
     }
 }
