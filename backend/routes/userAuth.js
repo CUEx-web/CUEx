c5a40175521d85c4c5211cb49d1363d77d7085b4
1155144002 <1155144002@link.cuhk.edu.hk>
2022-04-07 22:19:24 +0800

    Added get logged in user

--- backend/routes/userAuth.js
+++ backend/routes/userAuth.js
@@ -1,5 +1,7 @@
-const { authJwt } = require("../middlewares");
-const authController = require("../controllers/auth");
+const { authJwt } = require("../middlewares")
+const authController = require("../controllers/auth")
+const User = require("../models/user")
+
 module.exports = function(app) {
     app.use(function(req, res, next) {
         res.header(
@@ -9,10 +11,23 @@ module.exports = function(app) {
         next()
     })
     app.get("/api/test/all", authController.allAccess)
-    app.get("/api/test/user", [authJwt.verifyToken], authController.userBoard)
+    // app.get("/api/test/user", [authJwt.verifyToken], authController.userBoard)
     app.get(
         "/api/test/admin",
         [authJwt.verifyToken, authJwt.isAdmin],
         authController.adminBoard
-      )
+    )
+    app.get("/api/test/user", [authJwt.verifyToken], (req, res) => {
+        console.log(req.userId)
+        User.findById(req.userId)
+        .populate("productId", "_id productName")
+        .then(user => {
+            console.log(user)
+            return res.status(200).json(user)
+        })
+        .catch(err => {
+            console.log(err)
+            return res.status(500).json({ message: err.message })
+        })
+    })
  }
