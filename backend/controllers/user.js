c00f9138f5efb407e5d3bad6cfdcd4b6377cdd84
1155144002 <1155144002@link.cuhk.edu.hk>
2022-04-08 01:15:36 +0800

    Added Update and Delete user

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,10 @@
 const Users = require("../models/user")
 const mongoose = require('mongoose');
 
+//For delete image in server
+const fs = require('fs');
+const path = require('path');
+
 exports.getUsers = async (req, res) => {
     console.log(req.query)
     if (req.query.userName) {
@@ -32,7 +36,7 @@ exports.getUsers = async (req, res) => {
     if (req.query.studentId) {
         // Return user with their studentId matching req.query.studentId
         Users.find({ "studentId": req.query.studentId })
-            .populate("studentId", "_id productName")
+            .populate("productId", "_id productName")
             .then(user => {
                 console.log(user)
                 return res.status(200).json(user)
@@ -58,4 +62,154 @@ exports.getUsers = async (req, res) => {
         // Return error if wrong query is done
         return res.status(500).json({ message: "Call wrong api / Wrong query parameter" });
     }
+}
+
+exports.getUserById = async (req, res) => {
+    console.log(req.params)
+    const userId = req.params.userId;
+    Users.findById(userId)
+    .populate("userId", "_id userName")
+    .then(users => {
+        console.log(users);
+        //Return 200 and users if success get
+        return res.status(200).json(users);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ message: err.message });
+    });
+}
+
+//Small program to delete image in server
+const deleteImage = filePath => {
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, err => {
+        if (err) {
+            console.log(err)
+        }
+    })
+};
+
+exports.updateUser = async (req, res) => {
+    //Get userId from parms
+    const userId = req.params.userId;
+    Users.findById(userId).then(user => {
+        console.log(user)
+        //Frontend will give all info; changeable attributes distinguished by "req.body.*"
+        const userName = user.userName;
+        const email = req.body.email;
+        const userType = user.userType;
+        const studentId = user.studentId;
+        const status = user.status;
+        const confirmationCode = user.confirmationCode;
+        const registrationDate = user.registrationDate;
+        const password = req.body.password;
+        const productId = user.productId;
+        const grade = req.body.grade;
+        const profileDescription = req.body.profileDescription;
+        let profilePicture = "";
+        console.log(req.body);
+        //If update userImage, but fail to parse
+        if (req.files.profilePicture.length < 1) {
+            return res.status(500).json({
+                message:  "Fail to update user",
+                error: "The image is incorrect or missing!"
+            })
+        }
+        //Replace \ with /
+        if (req.files.profilePicture.length > 0) {
+            //Repalce with updated photo
+            profilePicture = req.files.profilePicture[0].path.replace(/\\/g,"/");
+        }
+        Users.findById(userId)
+            .then(user => {
+                if (!user) {
+                    return res.status(404).json({
+                        //If the user does not exist, return 404
+                        message:  "Fail to update user",
+                        error: "The user does not exist!"
+                    })
+                }
+                if (user._id.toString() != req.userId) {
+                    //If userId is not equal, then return 403
+                    return res.status(403).json({
+                        message: "You are not the user! Please login first."
+                    })
+                }
+                if (user.profilePicture != profilePicture) {
+                    //If new one not equal old one, delete the old image
+                    deleteImage(user.profilePicture);
+                }
+                //Update all info
+                user.userName = userName;
+                user.email = email;
+                user.userType = userType;
+                user.studentId = studentId;
+                user.status = status;
+                user.confirmationCode = confirmationCode;
+                user.registrationDate = registrationDate;
+                user.password = password;
+                user.productId = productId;
+                user.grade = grade;
+                user.profileDescription = profileDescription;
+                user.profilePicture = profilePicture;
+                console.log(user)
+                return user.save();
+            })
+            .then(updatedUser => {
+                return res.status(200).json({
+                    message: "User Updated!",
+                    user: updatedUser
+                });
+            })
+            .catch(err => {
+                console.log(err)
+                return res.status(500).json({
+                    message: "Fail to delete user",
+                    error: err
+                })
+            })
+        })
+}
+
+exports.deleteUser = async (req, res) => {
+    userId = req.params.userId;
+    console.log(userId)
+    Users.findById(userId)
+        .then(user => {
+            if (!user) {
+                return res.status(404).json({
+                    message: "Fail to find the user"
+                })
+            }
+            if (user._id.toString() != req.userId) {
+                return res.status(403).json({
+                    message: "You are not the user owner! Please login first"
+                })
+            }
+            //Delete the user image in server
+            deleteImage(user.profilePicture);
+            //Delete user info in mongoDB
+            console.log("OK")
+            return Users.findByIdAndRemove(userId);
+        })
+        // .then(result => {
+        //     return Users.findById(req.userId)
+        // })
+        // .then(user => {
+        //     user.productId.pull(userId);
+        //     return user.save();
+        // })
+        .then(result => {
+            return res.status(200).json({
+                message: "Deleted user with userId: " + userId
+            });
+        })
+        .catch(err => {
+            console.log(err)
+            return res.status(500).json({
+                message: "Fail to delete user",
+                error: err
+            })
+        })
 }
